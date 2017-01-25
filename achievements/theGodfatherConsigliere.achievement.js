0a80e96b44bda27a573806f544af8af6ea2f42b1
dunaevsky <duna87@gmail.com>
2017-01-25 20:11:33 +0200

    Contribute kibibit achievment + fix for Meeseek achievment (#34)
    * add Godfather achievment for users who contribute to Kibibit

---
+++ achievements/theGodfatherConsigliere.achievement.js
@@ -0,0 +1,20 @@
+var _ = require('lodash');
+
+var theGodfatherConsigliere = {
+	name: 'The Godfather Consigliere',
+	check: function(pullRequest, shall) {
+		if (_.result(pullRequest, 'organization.username') === 'Kibibit') {
+			var achievement = {
+				avatar : 'images/achievements/thegodfather.achievement.jpg',
+				name: 'The Godfather Consigliere',
+				short: 'Great men are not born great, they contribute to Kibibit . . . ',
+				description: 'You have contributed to Kibibit! We really appreciate it!<br>Accept this achievement as gift on my daughter\'s wedding day',
+				relatedPullRequest: pullRequest._id
+			};
+
+			shall.grant(pullRequest.creator.username, achievement);
+		}
+	}
+};
+
+module.exports = theGodfatherConsigliere;
