0920be14c84bf42c75df21c870ba6cc7b1669ce0
dunaevsky <duna87@gmail.com>
2017-01-25 17:23:57 +0200

    Small fix to Godfather

--- achievements/contribubit.achievement.js
+++ achievements/contribubit.achievement.js
@@ -4,13 +4,13 @@ var console = process.console;
 var TheGodfatherConsigliere = {
 	name: 'TheGodfatherConsigliere',
 	check: function(pullRequest, shall) {
-		if (pullRequest.organization.username === "TheDunaevsky") {
-			console.log("TheDunaevsky owns this...");
+		if (contributedToKibibit(pullRequest)) {
+			//console.log("TheDunaevsky owns this...");
 			var achievement = {
 				avatar : 'https://worldfilmgeek.files.wordpress.com/2016/12/thegodfatherstill.jpg',
 				name: 'The Godfather Consigliere',
 				short: 'Great men are not born great, they contribute to Kibibit . . . ',
-				description: 'Contributed to a Kibibit repository. Accept this achievement as gift on my daughter\'s wedding day',
+				description: 'You have contributed to a Kibibit repository. Accept this achievement as gift on my daughter\'s wedding day',
 				relatedPullRequest: pullRequest._id
 			};
 
@@ -18,5 +18,16 @@ var TheGodfatherConsigliere = {
 		}
 	}
 };
+
+function contributedToKibibit(pullRequest){
+	var organizationName = pullRequest.organization.username;
+	if ( typeof organizationName !== 'undefined'){
+		return organizationName === "Kibibit";
+	} else {
+		return false;
+	}
+
+
+}
 
 module.exports = TheGodfatherConsigliere;
