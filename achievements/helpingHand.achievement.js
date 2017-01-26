62d28d852541c50d8ac9f32a571b4c63cd491bf0
Neil Kalman <Neilkalman@gmail.com>
2017-01-26 20:43:28 +0200

    general fixes for helping hand achievement
    - don't give the creator the reviewer's achievement
    - remove the brackets for `isMultipleCommittedReviewers`
    - fix usernames in each other's achievements

--- achievements/helpingHand.achievement.js
+++ achievements/helpingHand.achievement.js
@@ -7,13 +7,16 @@ var helpingHand = {
 
     if (!_.isEmpty(committedReviewers)) {
 
-      var isMultipleCommittedReviewers = committedReviewers.length > 1 ? '(s) ' : ' ';
+      var isMultipleCommittedReviewers = committedReviewers.length > 1 ? 's ' : ' ';
 
       var reviewerAchievement = {
         avatar: 'images/achievements/helpingHandHelloThere.achievement.jpg',
         name: 'Helping Hand',
         short: 'Hello there. Slow going?',
-        description: 'You\'ve committed to ' + pullRequest.commit.author.username + '\'s Pull Request you are reviewing',
+        description: [
+          'You\'ve committed to ', pullRequest.creator.username,
+          '\'s Pull Request you are reviewing'
+        ].join(''),
         relatedPullRequest: pullRequest._id
       };
 
@@ -21,7 +24,11 @@ var helpingHand = {
         avatar: 'images/achievements/helpingHandManInBlack.achievement.jpg',
         name: 'Helping Hand',
         short: 'Look, I don\'t mean to be rude but this is not as easy as it looks',
-        description: 'Your reviewer' + isMultipleCommittedReviewers + committedReviewers.join(', ') +  ' committed to your Pull Request',
+        description: [
+          'Your reviewer', isMultipleCommittedReviewers,
+          _.map(committedReviewers, 'username').join(', '),
+          ' committed to your Pull Request'
+        ].join(''),
         relatedPullRequest: pullRequest._id
       };
 
@@ -39,7 +46,8 @@ function reviewersWhoCommittedToPullRequest(pullRequest) {
   var committedReviewers = [];
 
   _.forEach(pullRequest.commits, function(commit) {
-    if (_.find(pullRequest.reviewers, {username: commit.author.username})) {
+    if (commit.author.username !== pullRequest.creator.username &&
+      _.find(pullRequest.reviewers, {username: commit.author.username})) {
       committedReviewers.push(commit.author);
     }
   });
