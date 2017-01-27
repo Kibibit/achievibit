8ac8ef5fe7ae4b5c162e451883f11e8a6e432bae
dunaevsky <duna87@gmail.com>
2017-01-27 18:01:46 +0200

    godfather decription fix+label maker image path name (#46)

--- achievements/labelBabyJunior.achievement.js
+++ achievements/labelBabyJunior.achievement.js
@@ -3,7 +3,7 @@ var labelBabyJunior = {
 	check: function(pullRequest, shall) {
 		if (isManyLabels(pullRequest))  {
 			var achievement = {
-				avatar : 'images/achievements/theLabelMaker.achievement.jpg',
+				avatar : 'images/achievements/labelBabyJunior.achievement.jpg',
 				name: 'The Label Maker',
 				short: 'Is this a label maker?',
 				description: 'You\'ve put many labels, thank you for organizing. You\'re a gift that keeps on re-giving' ,
