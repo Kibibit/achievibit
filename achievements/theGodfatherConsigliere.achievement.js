f9d7ef4810ee1499649860f632ee1087e80806c4
Or Tichon <ortichon@users.noreply.github.com>
2017-01-27 04:02:18 +0200

    fixed image path and some lint (#42)

--- achievements/theGodfatherConsigliere.achievement.js
+++ achievements/theGodfatherConsigliere.achievement.js
@@ -4,11 +4,16 @@ var theGodfatherConsigliere = {
 	name: 'The Godfather Consigliere',
 	check: function(pullRequest, shall) {
 		if (_.result(pullRequest, 'organization.username') === 'Kibibit') {
+
 			var achievement = {
-				avatar : 'images/achievements/thegodfather.achievement.jpg',
+				avatar : 'images/achievements/theGodfatherConsigliere.achievement.jpg',
 				name: 'The Godfather Consigliere',
-				short: 'Great men are not born great, they contribute to Kibibit . . . ',
-				description: 'You have contributed to Kibibit! We really appreciate it!\nAccept this achievement as gift on my daughter\'s wedding day',
+				short: 'Great men are not born great, they contribute to Kibibit . . .',
+				description: [
+					'You have contributed to Kibibit! We really',
+					'appreciate it!\nAccept this achievement as gift on',
+					'my daughter\'s wedding day'
+				].join(''),
 				relatedPullRequest: pullRequest._id
 			};
 
