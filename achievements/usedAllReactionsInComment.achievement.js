71e944e77dd5e1cbef6ad443dd7cc770b455de3a
dunaevsky <duna87@gmail.com>
2017-01-27 22:05:55 +0200

    [ACHIEVEMENT] used all reactions
    This is not including inline comments Yet.
    Also, I've noticed there is no collection of reactions for the PR itself. Can we?
    The name will change. Also not sure about the beatles.

---
+++ achievements/usedAllReactionsInComment.achievement.js
@@ -0,0 +1,53 @@
+var _ = require('lodash');
+
+//Gives achievement to comment authors who got all reactions without reacting themselves
+
+var usedAllReactionsInComment = {
+	name: 'The Beatles',
+	check: function(pullRequest, shall) {
+    if (!_.isEmpty(pullRequest.comments)){
+      var topComments = commentsWithAllReactions(pullRequest.comments); //array of comments with all reactions in them
+    }
+
+		if ( !_.isEmpty(topComments) ) {
+
+			var achievement = {
+				avatar : '',
+				name: 'The Beatles',
+				short: 'All you need is love',
+				description: 'You got all the reactions to your comment. You make everybody feel.',
+				relatedPullRequest: pullRequest._id
+			};
+
+			_.forEach(topComments, function(comment) { //foreach comment that succeded give acheivemnt to author
+        shall.grant(comment.author.username, achieve);
+      });
+		}
+	}
+};
+
+
+function commentsWithAllReactions(comments) {
+  console.log("I'm inside: commentsWithAllReactions");
+  var allReactions = ['+1', '-1', 'laugh', 'confused', 'heart', 'hooray'];
+  console.log("here are all reactions: " + allReactions);
+  var topComments =[];
+
+  _.forEach(comments, function(comment) {//iterate over comments
+    if(!_.isEmpty(comments.reactions)){//if there are reactions
+      var localReactions = [];
+
+      _.forEach(comment.reactions, function(reaction){//if (comment has all reactions)
+        //if (reaction.user.username != comment.author.username) { //didn't react to self
+          localReactions.push(reaction.reaction);
+      //  }
+        console.log("localReactions after run:" +localReactions);
+      });
+      if (_.difference(allReactions, localReactions).length === 0) {//all reactions used in this comment
+        topComments.push(comment); //add comment to topComments
+      }
+    }
+  });
+}
+
+module.exports = usedAllReactionsInComment;
