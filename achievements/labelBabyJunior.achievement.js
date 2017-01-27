2dcacaee2bd5228894bab3353a445e9184bdf245
dunaevsky <duna87@gmail.com>
2017-01-27 17:11:21 +0200

    [ACHIEVEMENT] The label maker achievemt (#38)
    * The label maker achievemt
    
    Achievement for putting many labels in a pull request
    
    * label maker - removed notes
    
    * fix 2
    
    removed double check if labels defined
    
    * label maker ach - update 3
    
    func update, and line spacing
    
    * label maker - update 4
    
    added brackets, tested, and 6 labels are now minimum
    
    * label maker typo fixes+image rescale
    
    * label maker image ratio fix+name fix

---
+++ achievements/labelBabyJunior.achievement.js
@@ -0,0 +1,23 @@
+var labelBabyJunior = {
+	name: 'Label Baby Junior',
+	check: function(pullRequest, shall) {
+		if (isManyLabels(pullRequest))  {
+			var achievement = {
+				avatar : 'images/achievements/theLabelMaker.achievement.jpg',
+				name: 'The Label Maker',
+				short: 'Is this a label maker?',
+				description: 'You\'ve put many labels, thank you for organizing. You\'re a gift that keeps on re-giving' ,
+				relatedPullRequest: pullRequest._id
+			};
+
+			shall.grant(pullRequest.creator.username, achievement);
+		}
+	}
+};
+
+function isManyLabels(pullRequest) {
+	var labels = pullRequest.labels;
+	return labels && labels.length > 5;
+}
+
+module.exports = labelBabyJunior;
