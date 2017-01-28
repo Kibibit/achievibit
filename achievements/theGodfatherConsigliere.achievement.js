8b02886897037fd00a8608064fa69ad6c047e2fa
Neil Kalman <neilkalman@gmail.com>
2017-01-28 23:53:44 +0200

    change all \n to paragraphs in achievements

--- achievements/theGodfatherConsigliere.achievement.js
+++ achievements/theGodfatherConsigliere.achievement.js
@@ -10,9 +10,10 @@ var theGodfatherConsigliere = {
         name: 'The Godfather Consigliere',
         short: 'Great men are not born great, they contribute to Kibibit . . .',
         description: [
-          'You have contributed to Kibibit! We really ',
-          'appreciate it!\nAccept this achievement as gift on ',
-          'my daughter\'s wedding day'
+          '<p>You have contributed to Kibibit! We really ',
+          'appreciate it!</p>',
+          '<p>Accept this achievement as gift on ',
+          'my daughter\'s wedding day</p>'
         ].join(''),
         relatedPullRequest: pullRequest._id
       };
