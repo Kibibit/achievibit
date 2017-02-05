087204f5231d54f9ee695e8cbf05194243a26233
Or Tichon <or.tichon@gmail.com>
2017-02-05 02:13:39 +0000

    added suffix to JS

--- achievements/dontYellAtMe.achievement.js
+++ achievements/dontYellAtMe.achievement.js
@@ -1,6 +1,4 @@
-var moment = require('moment');
-
-var member = {
+var dontYellAtMe = {
   name: 'Don\'t Yell At Me!!!',
   check: function(pullRequest, shall) {
     var reason = isCreatorJustMean(pullRequest);
@@ -22,22 +20,26 @@ var member = {
 
 function isCreatorJustMean(pullRequest) {
   var isTitleContainLetters = /[a-zA-Z]/.test(pullRequest.title);
-  var isNoLowerCase = /^[^a-z]/.test(pullRequest.title);
+  var isNoLowerCase = /^[^a-z]*$/.test(pullRequest.title);
   var isOverExclamation = /!{3}/.test(pullRequest.title);
 
   var reason = '';
   var comboPotential = '';
 
-  if (isTitleContainLetters && isNoLowerCase) {
-    reason += 'ALL CAPS';
-    comboPotential = ' and ';
+  if (isTitleContainLetters) {
+    if (isNoLowerCase) {
+      reason += 'ALL CAPS';
+      comboPotential = ' and ';
+    }
+    
+    if (isOverExclamation) {
+      reason += comboPotential + '3 or more exclamation marks';
+    }
   }
 
-  if (isOverExclamation) {
-    reason += comboPotential + '3 or more exclamation marks';
-  }
+
 
   return reason;
 }
 
-module.exports = member;
+module.exports = dontYellAtMe;
