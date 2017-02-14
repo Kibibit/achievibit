9b2db7b0d4ed0e8484bf7a9e945f67f6355d5407
Neil Kalman <Neilkalman@gmail.com>
2017-02-14 11:36:08 +0200

    little nits

--- test/printLogo.specs.js
+++ test/printLogo.specs.js
@@ -6,9 +6,9 @@ describe('printLogo', function() {
   it('should print logo if called to console log', function() {
     stubHelper(function() {
       var didRun = false;
+      // mock console before requiring printLogo
       console.log = function() { didRun = true; };
 
-      // run the file
       var printLogo = require('../printLogo');
       printLogo();
 
