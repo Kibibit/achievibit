2b5fa299f17c13df090544a32c49c93ed7be476a
Neil Kalman <Neilkalman@gmail.com>
2017-02-14 02:11:37 +0200

    [ACHIEVEMENT] Mr. Miyagi (#74)
    * implement Mr Miyagi achievement
    
    * add tests for reactionOnEveryComment and fix the achievement
    
    * CR Comments

--- achievements/reactionOnEveryComment.achievement.js
+++ achievements/reactionOnEveryComment.achievement.js
@@ -3,16 +3,24 @@ var _ = require('lodash');
 var reactionOnEveryComment = {
   name: 'Royal Flush',
   check: function(pullRequest, shall) {
-    if (pullRequest.inlineComments.length > 0 &&
-				_.every(pullRequest.inlineComments, haveReactions) &&
-			pullRequest.comments.length > 0 &&
-			_.every(pullRequest.comments, haveReactions)) {
+    var doesCommentsExist =
+      _.get(pullRequest.inlineComments, 'length') > 0 ||
+      _.get(pullRequest.comments, 'length') > 0;
+    var isReactionOnEverything =
+      _.every(pullRequest.inlineComments, haveReactions) &&
+      _.every(pullRequest.comments, haveReactions) &&
+      _.every([ pullRequest ], haveReactions);
+
+    if (doesCommentsExist && isReactionOnEverything) {
 
       shall.grant(pullRequest.creator.username, {
         avatar: 'images/achievements/reactionOnEveryComment.achievement.png',
         name: 'royal flush',
         short: 'emojis on all of the comments',
-        description: 'got for having lots of emojis',
+        description: [
+          'got for having at least one comment\\inline comment, ',
+          'and all of them (including the PR description) had reactions'
+        ].join(''),
         relatedPullRequest: pullRequest.id
       });
 
@@ -21,7 +29,8 @@ var reactionOnEveryComment = {
 };
 
 function haveReactions(comment) {
-  return _.isArray(comment.reactions) && comment.reactions.length !== 0;
+  return _.isArray(comment.reactions) &&
+    _.get(comment.reactions, 'length') !== 0;
 }
 
 module.exports = reactionOnEveryComment;
