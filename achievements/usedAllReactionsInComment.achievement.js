b9e31d620dfad8e732ee85a92dbfa9c1222639f7
dunaevsky <duna87@gmail.com>
2017-02-26 00:12:35 +0200

    fix achievement + implement tests

--- achievements/usedAllReactionsInComment.achievement.js
+++ achievements/usedAllReactionsInComment.achievement.js
@@ -1,70 +1,64 @@
 var _ = require('lodash');
-var console = process.console;
-//Gives achievement to comment authors who got all reactions without reacting themselves
-//TODO: make it work for inline comments also
+// Gives achievement to comment authors who got all reactions
+// without reacting themselves
 
 var usedAllReactionsInComment = {
-	name: 'Gladiator',
-	check: function(pullRequest, shall) {
-    var topComments = objectsWithAllReactions(pullRequest); //array of comments/inline/pr with all reactions in them
-    if ( !_.isEmpty(topComments) ) {
-    var achievement = {
-      avatar : 'images/achievements/gladiator.achievement.gif',
-      name: 'Gladiator',
-      short: 'Are You Not Entertained?',
-      description: 'You got all the reactions to your comment. You make everybody feel.',
-      relatedPullRequest: pullRequest._id
-    };
+  name: 'Gladiator',
+  check: function(pullRequest, shall) {
+    var topAuthorsUsernames = getCommentAuthorsWithAllReactions(pullRequest);
+    if (!_.isEmpty(topAuthorsUsernames)) {
+      var achievement = {
+        avatar: 'images/achievements/gladiator.achievement.gif',
+        name: 'Gladiator',
+        short: 'Are You Not Entertained?',
+        description: [
+          'You got all the reactions to your comment. You make everybody feel.'
+        ].join(''),
+        relatedPullRequest: pullRequest._id
+      };
 
-    _.forEach(topComments, function(comment) { //foreach comment that succeded give acheivemnt to author
-      shall.grant(comment.author.username, achievement);
-    });
-  }
-	}
-};
-
-function appendIfHasAllReactions (topComments, comment){
-  if(!_.isEmpty(comment.reactions)){//if there are reactions
-    var allReactions = ['+1', '-1', 'laugh', 'confused', 'heart', 'hooray'];
-    var localReactions = [];
-    console.log("I'm inside: appendIfHasAllReactions");
-    _.forEach(comment.reactions, function(reaction){//colect reactions
-      if (reaction.user.username != comment.author.username) { //didn't react to self
-        localReactions.push(reaction.reaction);
-      }
-      console.log("localReactions after run:" +localReactions);
-    });
-    if (_.difference(allReactions, localReactions).length === 0) {//all reactions used in this comment
-      topComments.push(comment); //add comment to topComments
+      _.forEach(topAuthorsUsernames, function(author) {
+        shall.grant(author, achievement);
+      });
     }
   }
+};
+
+function getCommentAuthorsWithAllReactions(pullRequest) {
+  var allComments = _.concat(pullRequest.comments, pullRequest.inlineComments);
+  var authors = _.map(allComments, 'author.username');
+  var AllCommentsReactions = _.map(allComments, 'reactions');
+
+    // also add pull request description reactions
+  authors.push(pullRequest.creator.username);
+  AllCommentsReactions.push(pullRequest.reactions);
+
+  getOnlyUniqueReactionsWithoutAuthors(AllCommentsReactions, authors);
+
+  return onlyUsersWithAllReactions(authors, AllCommentsReactions);
 }
 
-function commentsWithAllReactions(pullRequest) {
-  console.log("I'm inside: commentsWithAllReactions");
-  
-  var comments = pullRequest.comments;
-  var inlineComments = pullRequest.inlineComments
-  var topComments =[]; //objects with all reactions are saved here (PR/comment/inline)
-
-//////////////If PR////////////////////////////
-  appendIfHasAllReactions(topComments, pullRequest);
-
-  ////////Iterate over comments//////////////////
-  if (!_.isEmpty(comments){
-    _.forEach(comments, function(comment) {
-      appendIfHasAllReactions(topComments, comment);
-    });
-  }
-
-  ///////////////////Iterate over inlineComments//////////////////
-  if (!_.isEmpty(comments){
-    _.forEach(inlineComments, function(inlineComments) {
-      appendIfHasAllReactions (topComments, inlineComments);
-    });
-  }
-
-  return topComments;
+function reactionsWithoutAuthor(reactions, author) {
+  return _.map(_.reject(reactions, ['user.username', author]), 'reaction');
 }
 
+function getOnlyUniqueReactionsWithoutAuthors(AllCommentsReactions, authors) {
+  _.forEach(AllCommentsReactions, function(reactions, index) {
+    AllCommentsReactions[index] =
+            _.uniq(reactionsWithoutAuthor(reactions, authors[index]));
+
+  });
+}
+
+function onlyUsersWithAllReactions(authors, AllCommentsReactions) {
+  var commentAuthorsWithAllReactions = [];
+
+  _.forEach(authors, function(author, index) {
+    if (AllCommentsReactions[index].length === 6) {
+      commentAuthorsWithAllReactions.push(author);
+    }
+  });
+
+  return commentAuthorsWithAllReactions;
+}
 module.exports = usedAllReactionsInComment;
