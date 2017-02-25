591100a8cf044af4aa138f70daced6f11efb32bc
Michael Dunaevsky <duna87@gmail.com>
2017-02-26 00:28:39 +0200

    [ACHIEVEMENT] The Gladiator - achievement for getting all the reaction to a comment (#51)
    * [ACHIEVEMENT] used all reactions
    
    This is not including inline comments Yet.
    Also, I've noticed there is no collection of reactions for the PR itself. Can we?
    The name will change. Also not sure about the beatles.
    
    * used all reactions - now with returning value in function
    
    * All reactions used achvmnt- not including inline comments
    
    * Used all reactions - add inline and PR check(still problem with PR)
    
    Can't distinguish between when trying to eliminate self reactions.
    
    Also granting to creator is not set yet
    
    * fix achievement + implement tests
    
    * CR comments

---
+++ achievements/template.achievement.js
@@ -0,0 +1,23 @@
+
+
+//var template = {
+//	name: ,
+//	check: function(pullRequest, shall) {
+//		if (...) {
+//
+//			var achievement = {
+//	/			avatar : '',
+	//			name: '',
+		//		short: '',
+			//	description: '',
+			//	relatedPullRequest: pullRequest._id
+		//	};
+
+			//shall.grant(pullRequest.creator.username, achievement);
+	//	}
+//	}
+//};
+
+
+
+//module.exports = template;
