601e114e99857c2e7490e91a661ef20ffe71be66
thatkookooguy <neilkalman@gmail.com>
2017-08-09 09:16:47 +0300

    Simplify our console service to create defaults (show time and location)

--- consoleService.js
+++ consoleService.js
@@ -1,29 +1,17 @@
-var scribeConsole = function(tag, colors, _console) {
-  var _ = require('lodash');
-  colors = colors ? colors : 'red';
-  return {
-    log: function() {
-      var self = _console.time().tag({msg: tag, colors: colors});
-      self.info.apply(self, arguments);
-    },
-    error: function() {
-      var self = _console.time().tag({msg: tag, colors: colors});
-      self.error.apply(self, arguments);
-    },
-    warn: function() {
-      var self = _console.time().tag({msg: tag, colors: colors});
-      self.warn.apply(self, arguments);
-    },
-    info: function() {
-      var self = _console.time().tag({msg: tag, colors: colors});
-      self.info.apply(self, arguments);
-    },
-    debug: function() {
-      var self = _console.time().tag({msg: tag, colors: colors});
-      self.debug.apply(self, arguments);
-    },
-    customConsole: _console
-  };
+var scribe = require('scribe-js')(); // used for logs
+var console = null;
+
+var achievibitConsole = function() {
+  if (!console) {
+    console = scribe.console({
+      console: {
+        alwaysTime: true,
+        alwaysLocation: true
+      }
+    });
+  }
+
+  return console;
 };
 
-module.exports = scribeConsole;
+module.exports = achievibitConsole;
