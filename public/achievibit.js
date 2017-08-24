7ea08ccd310bbb813a860a21e6adb76aa860b509
thatkookooguy <neilkalman@gmail.com>
2017-08-24 17:17:50 +0300

    start auth implementation
    -  create function to render UI changes
    - create functions to handle authentication and log out
    - created function as a base for handling user settings
    
    This should cover almost all cases for the new UI things needed.
    
    After finishing this basis, I'll make some css changes and maybe change the api to update settings instead of webhooking each repo separately

--- public/achievibit.js
+++ public/achievibit.js
@@ -2,6 +2,12 @@
   $(function() {
     // handle auth
 
+    var loggedInUser;
+
+    // listen to auth change to show user as logged in
+    // if token is present.
+    // if so, request full user from achievibit, and render the userState!
+
     function authenticate(
       firebaseToken,
       githubToken,
@@ -21,6 +27,7 @@
         .then(function (response) {
           // change UI based on result
           console.log(response);
+          changeUserStateUI(response);
         })
         .catch(function (error) {
           console.log(error);
@@ -57,6 +64,72 @@
       }
 
       return urlBase + '?' + ret.join('&');
+    }
+
+    function changeUserStateUI(user) {
+      if (!user) {
+        renderLogIn();
+      } else {
+        renderUserState(user);
+      }
+    }
+
+    function renderLogIn() {
+      var userState = $('user-state');
+      userState.html('<button class="login">login</button>');
+      userState.find('button.login').click(function() {
+        // firebase login
+        console.log('login user using oauth');
+        //authenticate()
+        //.then(function(user) {
+        //  renderUserState(user);
+        //});
+      });
+    }
+
+    function renderUserState(user) {
+      var userState = $('user-state');
+      userState.html([
+        '<img class="avatar" href=', user.avatar, '>',
+        '<span class="username">', user.username, '</span>',
+        '<button class="settings"></button>',
+        '<button class="logout">logout</button>'
+      ].join(''));
+
+      userState.find('button.settings').click(openSettingsDialog);
+      userState.find('button.logout').click(logoutUser);
+    }
+
+    function logoutUser() {
+      // logout user using firebase
+      then(function() {
+        renderUserState();
+      });
+    }
+
+    function openSettingsDialog() {
+      vex.dialog.open({
+        message: 'Enter your username and password:',
+        input: [ // should generate based on given settings
+          '<input name="username" type="text" ',
+          'placeholder="Username" required />',
+          '<input name="password" type="password" ',
+          'placeholder="Password" required />'
+        ].join(''),
+        buttons: [
+          $.extend({}, vex.dialog.buttons.YES, { text: 'Save' }),
+          $.extend({}, vex.dialog.buttons.NO, { text: 'Discard' })
+        ],
+        callback: function (data) {
+          if (!data) {
+            // discard everything
+            console.log('Settings changes discarded');
+          } else {
+            // send data to server to update user settings
+            console.log('Username', data.username, 'Password', data.password);
+          }
+        }
+      });
     }
 
   }); // end of document ready
