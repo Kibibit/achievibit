2631e44aa1532816111ddb90737a84bfb40c3b4b
Andrea Rosales <andrearosr@gmail.com>
2018-11-02 02:55:12 -0500

    Update contributing guidelines (#172)
    * Update contributing guidelines
    
    * Parse string value to boolean

--- index.js
+++ index.js
@@ -59,7 +59,7 @@ if (!port) {
 
 var publicFolder = __dirname + '/public';
 
-var enableNgrok = privateConfig.ngrok;
+var enableNgrok = (privateConfig.ngrok === undefined) ? true : JSON.parse(privateConfig.ngrok);
 var token = privateConfig.ngrokToken;
 
 //TEMP HEADERS FOR ANGULAR 2 TEST
