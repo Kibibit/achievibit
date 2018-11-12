55dd08d5714eeff02a60a39f67ef98cc0a7f3e1e
Gil Tichon <39461857+ZimGil@users.noreply.github.com>
2018-11-12 15:09:06 +0200

    [ACHIEVEMENT] IM THE DEVIL I LOVE METAL (#189)
    New Achievement: "I'm The Devil I Love Metal [resolves #166 ]
    Will grant an (awesome Tenacious D reference) achievement for posting PRs with ID of any sequence of more than 2 sixes (6)
    
    Example: PR number #66 would get the achievement, but #266 or #6669 won't

---
+++ test/imTheDevilILoveMetal.specs.js
@@ -0,0 +1,111 @@
+var imTheDevilILoveMetal = require('../achievements/imTheDevilILoveMetal.achievement');
+var expect = require('chai').expect;
+
+describe('imTheDevilILoveMetal achievement', function() {
+  it(
+    'should not grant if PR number is a sequence of numbers other than 6',
+    function() {
+      var testShall = new Shall();
+      var pullRequest = new PullRequest();
+      
+      pullRequest.number = 333;
+      
+      imTheDevilILoveMetal.check(pullRequest, testShall);
+      expect(testShall.grantedToUsername).to.not.exist;
+      expect(testShall.grantedToUsername).to.not.exist;
+    }
+  );
+
+  it(
+    'should be granted to PR creator if PR number is a sequence of 2 sixes',
+    function() {
+      var testShall = new Shall();
+      var pullRequest = new PullRequest();
+      
+      pullRequest.number = 66;
+      
+      imTheDevilILoveMetal.check(pullRequest, testShall);
+      expect(testShall.grantedToUsername).to.be.a('string');
+      expect(testShall.grantedToUsername).to.equal('creator');
+      expect(testShall.grantedAchievement).to.be.an('object');
+	}
+  );
+
+  it(
+    'should be granted to PR creator if PR number is a sequence of more then 2 sixes',
+    function() {
+      var testShall = new Shall();
+      var pullRequest = new PullRequest();
+      
+      pullRequest.number = 6666;
+      
+      imTheDevilILoveMetal.check(pullRequest, testShall);
+      expect(testShall.grantedToUsername).to.be.a('string');
+      expect(testShall.grantedToUsername).to.equal('creator');
+      expect(testShall.grantedAchievement).to.be.an('object');
+	}
+  );
+
+  it(
+    'should not grant if PR number is 6',
+    function() {
+      var testShall = new Shall();
+      var pullRequest = new PullRequest();
+      
+      pullRequest.number = 6;
+      
+      imTheDevilILoveMetal.check(pullRequest, testShall);
+      expect(testShall.grantedToUsername).to.not.exist;
+      expect(testShall.grantedToUsername).to.not.exist;
+	}
+  );
+
+  it(
+    'should not grand if PR number is not only a sequence of sixes',
+    function() {
+      var testShall = new Shall();
+      var pullRequest = new PullRequest();
+      
+      pullRequest.number = 1666;
+      
+      imTheDevilILoveMetal.check(pullRequest, testShall);
+      expect(testShall.grantedToUsername).to.not.exist;
+      expect(testShall.grantedToUsername).to.not.exist;
+	}
+  );
+
+  it(
+    'should not fail if PR number does not exist',
+    function() {
+      var testShall = new Shall();
+      var pullRequest = new PullRequest();
+      
+      imTheDevilILoveMetal.check(pullRequest, testShall);
+      expect(testShall.grantedToUsername).to.not.exist;
+      expect(testShall.grantedToUsername).to.not.exist;
+	}
+  );
+
+});
+
+function Shall() {
+  var self = this;
+
+  self.grantedAchievement = undefined;
+  self.grantedToUsername = undefined;
+
+  self.grant = function(username, achievementObject) {
+    self.grantedToUsername = username;
+    self.grantedAchievement = achievementObject;
+  };
+};
+
+function PullRequest() {
+  return {
+    'id': 'test',
+    'url': 'url',
+    'creator': {
+      'username': 'creator'
+    }
+  };
+};
