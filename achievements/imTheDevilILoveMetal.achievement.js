2cde4c6594139ade15e02696729e353238e80cf8
ZimGil <gil.tichon@gmail.com>
2018-11-12 13:46:13 +0200

    indentation

--- achievements/imTheDevilILoveMetal.achievement.js
+++ achievements/imTheDevilILoveMetal.achievement.js
@@ -1,29 +1,29 @@
 var imTheDevilILoveMetal = {
-	name: 'I\'m the Devil I Love Metal',
-	check: function(pullRequest, shall) {
-		if (isSequenceOfSixes(pullRequest.number)) {
+  name: 'I\'m the Devil I Love Metal',
+  check: function(pullRequest, shall) {
+    if (isSequenceOfSixes(pullRequest.number)) {
 
-			var achievement = {
-				avatar: 'images/achievements/iAmTheDevilILikeMetal.achievement.gif',
-				name: 'I\'m the Devil I Love Metal',
-				short: 'check this riff it\'s fucking tasty',
-				description: [
-					'Your pull request number ' + pullRequest.number,
-					' summoned the devil himself. ',
-					'I hope that code you submitted was a masterpiece, ',
-					'or you\'re gonna gargle mayonnaise'
-					].join(''),
-				relatedPullRequest: pullRequest.id
-			};
+      var achievement = {
+        avatar: 'images/achievements/iAmTheDevilILikeMetal.achievement.gif',
+        name: 'I\'m the Devil I Love Metal',
+        short: 'check this riff it\'s fucking tasty',
+        description: [
+          'Your pull request number ' + pullRequest.number,
+          ' summoned the devil himself. ',
+          'I hope that code you submitted was a masterpiece, ',
+          'or you\'re gonna gargle mayonnaise'
+          ].join(''),
+        relatedPullRequest: pullRequest.id
+      };
 
-			shall.grant(pullRequest.creator.username, achievement);
-		}
-	}
+      shall.grant(pullRequest.creator.username, achievement);
+    }
+  }
 };
 
 function isSequenceOfSixes(number) {
-	const sequenceOfSixesRegEx = new RegExp(/^6{2,}$/);
-	return sequenceOfSixesRegEx.test(number);
+  const sequenceOfSixesRegEx = new RegExp(/^6{2,}$/);
+  return sequenceOfSixesRegEx.test(number);
 }
 
 module.exports = imTheDevilILoveMetal;
