768c03e1aa87c2cff3aa856393d36bd605ff6f8a
thatkookooguy <neilkalman@gmail.com>
2019-12-09 17:31:29 +0200

    feature: integrate swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,37 @@
-import { NestFactory } from '@nestjs/core';
+import { NestFactory, NestApplication } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import { ValidationPipe } from '@nestjs/common';
+import { join } from 'path';
+import { readFileSync } from 'fs-extra';
+import { NestExpressApplication } from '@nestjs/platform-express';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule);
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true
+  }));
+
+  const options = new DocumentBuilder()
+    .setTitle('achievibit')
+    .setDescription('The achievibit API description')
+    .setVersion('1.0')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, options);
+  // SwaggerModule.setup('api/docs', app, document);
+
+  SwaggerModule.setup('api/docs', app, document, {
+    customSiteTitle: `kibibit - achievibit API documentation`,
+    customCss: readFileSync(join(__dirname, '../public/swagger.css'), 'utf8'),
+    // customJs: '../swagger-things/swagger.js',
+    // customfavIcon: '../swagger-things/favicon-32.png'
+  });
+
+  app.useStaticAssets(join(__dirname, '..', 'public'), { prefix: '/public/' });
+  app.setBaseViewsDir(join(__dirname, '..', 'views'));
+  app.setViewEngine('hbs');
+
+  await app.listen(10101);
 }
 bootstrap();
