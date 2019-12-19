15f3ee6bad69c224b1d564eea8a806fa9f6cf2d3
thatkookooguy <neilkalman@gmail.com>
2019-12-19 17:41:25 +0200

    chore(test-results): add a comment to the pull request

--- deploy-test-results.js
+++ deploy-test-results.js
@@ -14,14 +14,32 @@ if (!travisPullRequest ||
 }
 
 exec(`now alias --token=${nowToken} $(now ./test-results --no-clipboard --token=${nowToken} --public) achievibit-pr-${ travisPullRequest }`,
-  (err, stdout, stderr) => {
+  async (err, stdout, stderr) => {
     if (err) {
       console.error(err);
       // node couldn't execute the command
       return;
     }
 
+    const Octokit = require("@octokit/rest");
+    const auth = process.env.GH_TOKEN;
+    const octokit = new Octokit({ auth });
+
+    const prSlug = process.env.TRAVIS_PULL_REQUEST_SLUG;
+
+    const splitted = prSlug.split('\/');
+
+    const comment = {
+      repo: splitted[1],
+      owner: splitted[0],
+      issue_number: travisPullRequest,
+      body: 'very nice, sir!'
+    };
+
+    const response = await octokit.issues.createComment(comment);
+
     // the *entire* stdout and stderr (buffered)
     console.log(`stdout: ${stdout}`);
     console.log(`stderr: ${stderr}`);
+    console.log(response);
   });
