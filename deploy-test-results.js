21efc6cd01c68cc5a4eb28aad193b87c53677ae6
thatkookooguy <neilkalman@gmail.com>
2019-12-19 18:03:34 +0200

    ci(test-results): prettify the comment left by k1b1b0t on test reports

--- deploy-test-results.js
+++ deploy-test-results.js
@@ -40,13 +40,13 @@ exec(`now alias --token=${nowToken} $(now ./test-results --no-clipboard --token=
       repo: splitted[1],
       owner: splitted[0],
       issue_number: travisPullRequest,
-      body: 'Test report is available on ' + testReportUrl
+      body: ':traffic_light: Test report is available on ' + testReportUrl
     };
 
     const response = await octokit.issues.createComment(comment);
 
     // the *entire* stdout and stderr (buffered)
-    console.log(`stdout: ${stdout}`);
-    console.log(`stderr: ${stderr}`);
-    console.log(response);
+    console.log(`${stdout}`);
+    console.log(`${stderr}`);
+    console.log(`~~ GH PR # ${ travisPullRequest } test report comment ~~`, response.data);
   });
