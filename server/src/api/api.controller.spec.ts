719fe270e2ab10a8adc968d86cc360c7c18c63fa
thatkookooguy <neilkalman@gmail.com>
2021-04-12 23:40:58 +0000

    test(api): add getApi test
    we should probably also test omitted attributes at some point

--- server/src/api/api.controller.spec.ts
+++ server/src/api/api.controller.spec.ts
@@ -1,11 +1,18 @@
 import { Test, TestingModule } from '@nestjs/testing';
+import fs from 'fs-extra';
 
 import { ApiController } from '@kb-api';
 
+jest.mock('fs-extra');
+jest.mock('@kb-app');
+
 describe('ApiController', () => {
+  // const MockedAppService = mocked(AppService, true);
   let controller: ApiController;
 
   beforeEach(async () => {
+    // Clears the record of calls to the mock constructor function and its methods
+    // MockedAppService.mockClear();
     const module: TestingModule = await Test.createTestingModule({
       controllers: [ApiController]
     }).compile();
@@ -17,7 +24,20 @@ describe('ApiController', () => {
     expect(controller).toBeDefined();
   });
 
-  it.skip('should return package.json object', () => {
-    expect(controller.getAPI()).toBe('API');
+  it('should return package.json object', async () => {
+    const packageInfo = {
+      name: 'nice',
+      description: 'nice',
+      version: 'nice',
+      license: 'nice',
+      repository: 'nice',
+      author: 'nice',
+      bugs: 'nice'
+    };
+    fs.readJSON = jest.fn().mockResolvedValue(packageInfo);
+    jest.spyOn(fs, 'readJSON');
+    expect(await controller.getAPI()).toEqual(packageInfo);
+    expect(fs.readJSON).toHaveBeenCalledTimes(1);
+    expect(fs.readJSON).toHaveBeenCalledWith('app-root/package.json');
   });
 });
