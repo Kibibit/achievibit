3d419a9eccfb30ab84a29696f103d9fd28795dfa
thatkookooguy <neilkalman@gmail.com>
2021-04-12 01:22:41 +0000

    chore(tests): added test tools for unit-tests
    mock module to mock dtos, and an in memory database to test services

---
+++ server/src/dev-tools/dto.mock-generator.ts
@@ -0,0 +1,86 @@
+import { Chance } from 'chance';
+import { kebabCase, times } from 'lodash';
+import { ObjectId } from 'mongodb';
+import RandExp from 'randexp';
+
+import { Repo, User } from '@kb-models';
+
+interface IChanceMixin {
+  mongoObjectId: () => ObjectId;
+  mongodbUrl: () => string;
+  user: () => User;
+  users: () => User[];
+  repo: () => Repo;
+  repos: () => Repo[];
+  achievementScripts: (numOfAchievements?: number) => { [ key: string ]: any };
+  randexp: (regex: RegExp) => RandExp;
+}
+
+const chance = new Chance() as Chance.Chance & IChanceMixin;
+
+export const DtoMockGenerator: Chance.Chance & IChanceMixin =
+  chance as Chance.Chance & IChanceMixin;
+
+const customMixin: IChanceMixin & Chance.MixinDescriptor = {
+  mongodbUrl(): string {
+    return chance.url({ protocol: 'mongodb' + (chance.bool() ? '+srv' : '') });
+  },
+  randexp(regex: RegExp): RandExp {
+    const randExp = new RandExp(regex);
+    randExp.max = 10;
+
+    return randExp;
+  },
+  mongoObjectId(): ObjectId {
+    return new ObjectId(chance.string({
+      pool: 'abcdefABCDEF123456789',
+      length: 24
+    }));
+  },
+  user(): User {
+    const isOrg = chance.bool();
+
+    return new User({
+      _id: chance.mongoObjectId(),
+      username: chance.name(),
+      avatar: chance.url(),
+      organization: isOrg,
+      repos: times(chance.integer({ min: 0, max: 10 }), () => chance.animal()),
+      url: chance.url(),
+      users: isOrg ?
+        times(chance.integer({ min: 0, max: 10 }), () => chance.animal()) :
+        undefined,
+      achievements: [],
+      token: chance.string()
+    });
+  },
+  users(): User[] {
+    return times(chance.integer({ min: 0, max: 10 }), () => chance.user());
+  },
+  repo(): Repo {
+    return new Repo({
+      _id: chance.mongoObjectId(),
+      name: chance.string(),
+      fullname: chance.string(),
+      organization: chance.company(),
+      url: chance.url()
+    });
+  },
+  repos(): Repo[] {
+    return times(chance.integer({ min: 0, max: 10 }), () => chance.repo());
+  },
+  achievementScripts(
+    numOfAchievements: number = chance.integer({ min: 1, max: 25 })
+  ): { [ key: string ]: any } {
+    const scriptsObject = {};
+
+    times(
+      numOfAchievements,
+      () => scriptsObject[ `${ kebabCase(chance.animal()) }.achievement` ] = {}
+    );
+
+    return scriptsObject;
+  }
+};
+
+chance.mixin(customMixin);
---
+++ server/src/dev-tools/in-memory-database.module.ts
@@ -0,0 +1,34 @@
+import { Module } from '@nestjs/common';
+import { MongooseModule } from '@nestjs/mongoose';
+import { MongoMemoryServer } from 'mongodb-memory-server';
+import mongoose from 'mongoose';
+
+let mongod: MongoMemoryServer;
+let uri: string;
+
+async function testDBFactory() {
+  mongod = new MongoMemoryServer();
+  uri = await mongod.getUri();
+  await mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  });
+  return { uri };
+}
+
+@Module({
+  imports: [
+    MongooseModule.forRootAsync({
+      useFactory: testDBFactory 
+    })
+  ]
+})
+export class InMemoryDatabaseModule {
+  static async closeDatabase() {
+    // await mongoose.connection.dropDatabase();
+    await mongoose.connection.close();
+    await mongod.stop();
+    mongod = null;
+    uri = null;
+  }
+}
