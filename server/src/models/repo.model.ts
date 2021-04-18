5da80ffcecd10df26d4cf737391e28dec920c9b0
thatkookooguy <neilkalman@gmail.com>
2021-04-18 21:32:55 +0000

    feat(models): add indexes and uniqueness to db
    username for user and fullname for repo should act as identifiers and should be unique

--- server/src/models/repo.model.ts
+++ server/src/models/repo.model.ts
@@ -1,29 +1,36 @@
 import { ApiProperty } from '@nestjs/swagger';
-import { prop as PersistInDb } from '@typegoose/typegoose';
+import { index, modelOptions, prop as PersistInDb } from '@typegoose/typegoose';
 import { Exclude, Expose } from 'class-transformer';
 import { IsNotEmpty, IsOptional, IsString } from 'class-validator';
 
 import { BaseModel } from '@kb-abstracts';
 
 @Exclude()
+@modelOptions({
+  schemaOptions: {
+    collation: { locale: 'en_US', strength: 2 },
+    timestamps: true
+  }
+})
+@index({ fullname: 1 }, { unique: true })
 export class Repo extends BaseModel {
 
   @Expose()
   @ApiProperty()
   @IsNotEmpty()
-  @PersistInDb()
+  @PersistInDb({ required: true })
   readonly name: string;
 
   @Expose()
   @ApiProperty()
   @IsNotEmpty()
-  @PersistInDb()
+  @PersistInDb({ required: true, unique: true })
   readonly fullname: string;
 
   @Expose()
   @ApiProperty()
   @IsString()
-  @PersistInDb()
+  @PersistInDb({ required: true })
   readonly url: string;
 
   @Expose()
