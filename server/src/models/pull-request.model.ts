b307eb7f9b01e3a5c26df4c219b78647ad97a9ec
thatkookooguy <neilkalman@gmail.com>
2021-04-18 23:45:13 +0000

    feat(engines): implement abstract engine and add pr model
    created an abstarct engine, created interfaces for github events, and created a pr model for
    tracking pr changes for achievements

---
+++ server/src/models/pull-request.model.ts
@@ -0,0 +1,80 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { index, modelOptions, prop as PersistInDb } from '@typegoose/typegoose';
+import { Exclude, Expose } from 'class-transformer';
+import { IsNumber, IsOptional, IsString } from 'class-validator';
+
+import { BaseModel } from '../abstracts/base.model.abstract';
+
+@Exclude()
+@modelOptions({
+  schemaOptions: {
+    collation: { locale: 'en_US', strength: 2 },
+    timestamps: true
+  }
+})
+@index({ id: 1 }, { unique: true })
+export class PullRequest extends BaseModel {
+  
+  @Expose()
+  @IsString()
+  @ApiProperty()
+  @PersistInDb({ required: true, unique: true })
+  id: string;
+
+  @Expose()
+  @IsString()
+  @PersistInDb({ required: true })
+  url: string;
+
+  @Expose()
+  @IsNumber()
+  @PersistInDb({ required: true })
+  number: number;
+
+  @Expose()
+  @IsString()
+  @PersistInDb()
+  title: string;
+
+  @Expose()
+  @IsString()
+  @PersistInDb()
+  description: string;
+
+  @Expose()
+  @PersistInDb()
+  creator: string;
+
+  @Expose()
+  @PersistInDb()
+  createdOn: Date;
+
+  @Expose()
+  @PersistInDb()
+  labels: any[];
+
+  @Expose()
+  @PersistInDb()
+  history: {
+    [ key: string ]: any;
+  };
+
+  @Expose()
+  @PersistInDb()
+  repository: string;
+
+  @Expose()
+  @IsOptional()
+  @PersistInDb()
+  organization?: string;
+
+  @Expose()
+  @IsOptional()
+  @PersistInDb()
+  assignees?: string[];
+
+  constructor(partial: Partial<PullRequest> = {}) {
+    super();
+    Object.assign(this, partial);
+  }
+}
