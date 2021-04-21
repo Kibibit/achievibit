641e8c36d943f44afa2caa79af7e8a2c278eaaf2
thatkookooguy <neilkalman@gmail.com>
2021-04-21 13:10:28 +0000

    test(api): start implementing tests for webhooks manager
    this uses the github captured data. still need to make the file itself more readable with the table
    datastructure from jest. it's a little messy atm

--- server/src/api/webhook-event-manager/webhook-event-manager.service.spec.ts
+++ server/src/api/webhook-event-manager/webhook-event-manager.service.spec.ts
@@ -1,11 +1,72 @@
 import { Test, TestingModule } from '@nestjs/testing';
 
-import { DtoMockGenerator } from '@kb-dev-tools';
+import { AchievibitEventNames } from '@kb-abstracts';
+import {
+  DtoMockGenerator,
+  pullRequestAssigneeAddedEvent,
+  pullRequestAssigneeRemovedEvent,
+  pullRequestCreatedEvent,
+  pullRequestEditedEvent,
+  pullRequestLabelAddedEvent,
+  pullRequestLabelRemovedEvent,
+  pullRequestLabelsInitializedEvent,
+  pullRequestMergedEvent,
+  pullRequestReviewRequestRemovedEvent,
+  pullReuqestReviewRequestAddedEvent,
+  reviewCommentAddedEvent,
+  reviewCommentEditedEvent,
+  reviewCommentRemovedEvent,
+  reviewSubmittedEvent,
+  webhookPingEvent
+} from '@kb-dev-tools';
+import { GithubEngine } from '@kb-engines';
 
 import { PullRequestService } from '../pull-request/pull-request.service';
 import { RepoService } from '../repo/repo.service';
 import { UserService } from '../user/user.service';
 import { WebhookEventManagerService } from './webhook-event-manager.service';
+
+const PullRequestEventsCases = [
+  [AchievibitEventNames.NewConnection, webhookPingEvent],
+  [AchievibitEventNames.PullRequestOpened, pullRequestCreatedEvent],
+  [AchievibitEventNames.PullRequestEdited, pullRequestEditedEvent],
+  [
+    AchievibitEventNames.PullRequestInitialLabeled,
+    pullRequestLabelsInitializedEvent
+  ],
+  [AchievibitEventNames.PullRequestLableAdded, pullRequestLabelAddedEvent],
+  [AchievibitEventNames.PullRequestLabelRemoved, pullRequestLabelRemovedEvent],
+  [
+    AchievibitEventNames.PullRequestAssigneeAdded,
+    pullRequestAssigneeAddedEvent
+  ],
+  [
+    AchievibitEventNames.PullRequestAssigneeRemoved,
+    pullRequestAssigneeRemovedEvent
+  ],
+  [AchievibitEventNames.PullRequestMerged, pullRequestMergedEvent]
+];
+
+const PullRequestReviewEventsCases = [
+  [
+    AchievibitEventNames.PullRequestReviewRequestAdded,
+    pullReuqestReviewRequestAddedEvent
+  ],
+  [
+    AchievibitEventNames.PullRequestReviewRequestRemoved,
+    pullRequestReviewRequestRemovedEvent
+  ],
+  [AchievibitEventNames.PullRequestReviewCommentAdded, reviewCommentAddedEvent],
+  [
+    AchievibitEventNames.PullRequestReviewCommentRemoved,
+    reviewCommentRemovedEvent
+  ],
+  [
+    AchievibitEventNames.PullRequestReviewCommentEdited,
+    reviewCommentEditedEvent
+  ],
+  [AchievibitEventNames.PullRequestReviewSubmitted, reviewSubmittedEvent]
+];
 
 describe('WebhookEventManagerService', () => {
   let service: WebhookEventManagerService;
@@ -38,5 +99,68 @@ describe('WebhookEventManagerService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('Translate GitHubEvents to AchievibitEvents', () => {
+    describe('Translate to internal events', () => {
+      it.each(PullRequestEventsCases)(
+        'should handle %p event',
+        (internalEventName, webhookData: any) => {
+          const result = service.translateToEventName(
+            webhookData.event,
+            webhookData.payload
+          );
+
+          expect(result).toBe(internalEventName);
+      });
+    });
+
+    describe('Pull Request Review Events', () => {
+      it.each(PullRequestReviewEventsCases)(
+        'should handle %p event',
+        (internalEventName, webhookData: any) => {
+          const result = service.translateToEventName(
+            webhookData.event,
+            webhookData.payload
+          );
+
+          expect(result).toBe(internalEventName);
+      });
+
+      it.todo('Review Edited Event');
+    });
+  });
+
+  describe('notifyAchievements', () => {
+    it('should catch new connections', async () => {
+      jest.spyOn(GithubEngine.prototype, 'handleNewConnection')
+        .mockImplementation(() => Promise.resolve());
+      await service
+        .notifyAchievements(webhookPingEvent.event, webhookPingEvent.payload);
+
+      expect(service.githubEngine.handleNewConnection).toHaveBeenCalledWith(
+        webhookPingEvent.payload
+      );
+      expect(service.githubEngine.handleNewConnection)
+        .toHaveBeenCalledTimes(1);
+    });
+
+    it('should catch open pull request', async () => {
+      jest.spyOn(GithubEngine.prototype, 'handlePullRequestOpened')
+        .mockImplementation(() => Promise.resolve());
+      await service
+        .notifyAchievements(
+          pullRequestCreatedEvent.event,
+          pullRequestCreatedEvent.payload
+        );
+
+      expect(service.githubEngine.handlePullRequestOpened).toHaveBeenCalledWith(
+        pullRequestCreatedEvent.payload
+      );
+      expect(service.githubEngine.handlePullRequestOpened)
+        .toHaveBeenCalledTimes(1);
+    });
+
+    
   });
 });
