ead9fc6db37689e5eef089d466d1967c311b93d4
Neil Kalman <Neilkalman@gmail.com>
2021-04-28 19:54:22 +0000

    test(coverage): cover more pr service methods and fix config test

--- server/src/api/pull-request/pull-request.service.spec.ts
+++ server/src/api/pull-request/pull-request.service.spec.ts
@@ -3,7 +3,8 @@ import { Test, TestingModule } from '@nestjs/testing';
 
 import {
   closeInMemoryDatabaseConnection,
-  createInMemoryDatabaseModule
+  createInMemoryDatabaseModule,
+  DtoMockGenerator
 } from '@kb-dev-tools';
 import { PullRequest } from '@kb-models';
 
@@ -33,5 +34,79 @@ describe('PullRequestService', () => {
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  it('should add and remove labels', async () => {
+    const mockPr = DtoMockGenerator.pullRequest();
+    await service.create(mockPr);
+    await service.addLabels(mockPr.prid, 'pizza', true);
+    await service.addLabels(mockPr.prid, 'lazagna');
+    let dbPR = await service.findOneAsync({ prid: mockPr.prid });
+    let pr = new PullRequest(dbPR.toObject());
+    expect(pr.labels).toContain('pizza');
+    expect(pr.labels).toContain('lazagna');
+
+    await service.removeLabels(mockPr.prid, 'pizza');
+    dbPR = await service.findOneAsync({ prid: mockPr.prid });
+    pr = new PullRequest(dbPR.toObject());
+    expect(pr.labels).not.toContain('pizza');
+    expect(pr.labels).toContain('lazagna');
+
+    await service.removeLabels(mockPr.prid, 'lazagna');
+    dbPR = await service.findOneAsync({ prid: mockPr.prid });
+    pr = new PullRequest(dbPR.toObject());
+    expect(pr.labels).not.toContain('pizza');
+    expect(pr.labels).not.toContain('lazagna');
+  });
+
+  it('should edit PR title and body', async () => {
+    const mockPr = DtoMockGenerator.pullRequest();
+    const newTitle = DtoMockGenerator.string();
+    const newDescription = DtoMockGenerator.paragraph();
+    await service.create(mockPr);
+    await service.editPRData(mockPr.prid, {
+      title: newTitle
+    }, {
+      title: { from: mockPr.title }
+    });
+    let dbPR = await service.findOneAsync({ prid: mockPr.prid });
+    let pr = new PullRequest(dbPR.toObject());
+    expect(pr.title).toBe(newTitle);
+    expect(pr.history.title).toBeDefined();
+
+    await service.editPRData(mockPr.prid, {
+      description: newDescription
+    }, {
+      body: { from: mockPr.description }
+    });
+    dbPR = await service.findOneAsync({ prid: mockPr.prid });
+    pr = new PullRequest(dbPR.toObject());
+    expect(pr.description).toBe(newDescription);
+    expect(pr.history.description).toBeDefined();
+  });
+
+  it('should edit PR assignees', async () => {
+    const mockPr = DtoMockGenerator.pullRequest();
+    const mockAssignees = DtoMockGenerator.users();
+    const mockUsernames = mockAssignees.map((user) => user.username);
+    await service.create(mockPr);
+    await service.updateAssignees(mockPr.prid, mockAssignees);
+    const dbPR = await service.findOneAsync({ prid: mockPr.prid });
+    const pr = new PullRequest(dbPR.toObject());
+    expect(pr.assignees).toEqual(expect.arrayContaining(mockUsernames));
+  });
+
+  it('should edit PR reviewers', async () => {
+    const mockPr = DtoMockGenerator.pullRequest();
+    const mockReviewer = DtoMockGenerator.user();
+    await service.create(mockPr);
+    await service.updateReviewers(mockPr.prid, mockReviewer);
+    let dbPR = await service.findOneAsync({ prid: mockPr.prid });
+    let pr = new PullRequest(dbPR.toObject());
+    expect(pr.reviewers).toContain(mockReviewer.username);
+    await service.updateReviewers(mockPr.prid, mockReviewer, true);
+    dbPR = await service.findOneAsync({ prid: mockPr.prid });
+    pr = new PullRequest(dbPR.toObject());
+    expect(pr.reviewers).not.toContain(mockReviewer.username);
   });
 });
