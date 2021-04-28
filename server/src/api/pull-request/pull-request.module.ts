11500add6b667263e21baf395feb49196ff55dbc
Neil Kalman <Neilkalman@gmail.com>
2021-04-28 13:12:20 +0000

    feat(api): only load pull request controller in dev environments
    This will allow developers to get pull requests from the db through the api,
    but will keep that API hidden in production where we don't want
    people to access data on pull requests

--- server/src/api/pull-request/pull-request.module.ts
+++ server/src/api/pull-request/pull-request.module.ts
@@ -1,10 +1,25 @@
-import { Module } from '@nestjs/common';
+import { Logger, Module, Type } from '@nestjs/common';
 import { MongooseModule } from '@nestjs/mongoose';
 
+import { ConfigService } from '@kb-config';
 import { PullRequest } from '@kb-models';
 
 import { PullRequestController } from './pull-request.controller';
 import { PullRequestService } from './pull-request.service';
+
+const devControllers: Type<any>[] = [PullRequestController];
+const logger = new Logger('PullRequestModule');
+
+const config = new ConfigService();
+const controllers = (() => {
+  if (config.nodeEnv === 'production') {
+    return [];
+  } else {
+    logger.log('Not running in production mode!');
+    logger.warn('Attaching Pull Request controller for development');
+    return devControllers;
+  }
+})();
 
 @Module({
   imports: [
@@ -13,7 +28,7 @@ import { PullRequestService } from './pull-request.service';
     ])
   ],
   providers: [PullRequestService],
-  controllers: [PullRequestController],
+  controllers,
   exports: [PullRequestService]
 })
 export class PullRequestModule {}
