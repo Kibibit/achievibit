37a671ef0c98b2be9449315b26812acf45a710b8
Neil Kalman <Neilkalman@gmail.com>
2021-05-05 00:09:31 +0000

    build(eslint): sort imports as part of eslint --fix

--- server/.eslintrc.js
+++ server/.eslintrc.js
@@ -4,7 +4,11 @@ module.exports = {
     project: 'tsconfig.json',
     sourceType: 'module',
   },
-  plugins: ['@typescript-eslint/eslint-plugin'],
+  plugins: [
+    '@typescript-eslint/eslint-plugin',
+    'simple-import-sort',
+    'import'
+  ],
   extends: [
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',
@@ -19,6 +23,30 @@ module.exports = {
     jest: true,
   },
   rules: {
+    'simple-import-sort/imports': ['error', {
+      groups: [
+        // 1. built-in node.js modules
+        [`^(${require("module").builtinModules.join("|")})(/|$)`],
+        // 2.1. package that start without @
+        // 2.2. package that start with @ 
+        ['^\\w', '^@\\w'],
+        // 3. @nestjs packages
+        ['^@nestjs\/'],
+        // 4. @kibibit external packages
+        ['^@kibibit\/'],
+        // 5. Internal kibibit packages (inside this project)
+        ['^@kb-'],
+        // 6. Parent imports. Put `..` last.
+        //    Other relative imports. Put same-folder imports and `.` last.
+        ["^\\.\\.(?!/?$)", "^\\.\\./?$", "^\\./(?=.*/)(?!/?$)", "^\\.(?!/?$)", "^\\./?$"],
+        // 7. Side effect imports.
+        // https://riptutorial.com/javascript/example/1618/importing-with-side-effects
+        ["^\\u0000"]
+      ]
+    }],
+    'import/first': 'error',
+    'import/newline-after-import': 'error',
+    'import/no-duplicates': 'error',
     'eol-last': [ 2, 'windows' ],
     'comma-dangle': [ 'error', 'never' ],
     'max-len': [ 'error', { 'code': 80, "ignoreComments": true } ],
@@ -37,6 +65,8 @@ module.exports = {
           "match": true
         }
       }
-    ]
+    ],
+    "semi": "off",
+    "@typescript-eslint/semi": ["error"]
   }
 };
